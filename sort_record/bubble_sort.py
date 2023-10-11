c4d2c6ffee4840ee285b8c37be66abb59f6c0337
FanCaiJian <fancaijian@mozat.com>
2023-10-11 14:21:24 +0800

    add mark

--- sort_record/bubble_sort.py
+++ sort_record/bubble_sort.py
@@ -5,6 +5,7 @@ def bubble_sort(nums: list[int]):
     """冒泡排序"""
     n = len(nums)
     for i in range(n-1, 0, -1):
+        # 内循环：将未排序区间 [0, i] 中的最大元素交换至该区间的最右端
         for j in range(i):
             if nums[j] > nums[j+1]:
                 nums[j+1], nums[j] = nums[j], nums[j+1]
